f0e0be8534d03b75957a525f6606e989fb10774f
Binil-Dev <binildev414@gmail.com>
2024-06-06 21:39:30 +0530

    Updated android folder

--- lib/features/pharmacy/presentation/pharmacy_main.dart
+++ lib/features/pharmacy/presentation/pharmacy_main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:healthy_cart_user/core/custom/app_bars/home_sliver_appbar.dart';
 
 class PharmacyMain extends StatelessWidget {
   const PharmacyMain({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Pharmacy'),
-      ),
+    return CustomScrollView(
+      slivers: [
+        HomeSliverAppbar(searchHint: 'Search Pharmacy', searchController: TextEditingController(), onChanged: (_){},)
+        
+      ],
     );
   }
 }
