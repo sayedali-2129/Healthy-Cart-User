a06bb3ce6265b02a64be1193406e7b9e5ef60236
sayedali-2129 <sayedalipld@gmail.com>
2024-07-17 01:50:52 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -30,9 +31,9 @@ const AndroidNotificationChannel channel = AndroidNotificationChannel(
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
-// Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   await configureDependancy();
-// }
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await configureDependancy();
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
